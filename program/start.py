419ce84fdbd72c75104b2f8aaaae08bb4c8e4fb6
USDDBOT <101028273+USDDBOT@users.noreply.github.com>
2022-03-24 18:25:02 +0200

    Update start.py

--- program/start.py
+++ program/start.py
@@ -52,12 +52,11 @@ async def _human_time_duration(seconds):
     return ", ".join(parts)
 
 
-@Client.on_message(
-    command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited
-)
+@Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
-    await message.reply_text(
-        f"""✨ **مرحبا عزيزي ↤ {message.from_user.mention()} !**\n
+    await message.reply_photo(
+        photo=f"{BOT_USERNAME}",,
+        caption=f"""✨ **مرحبا عزيزي« {message.from_user.mention()} !**\n
 💭 **انا بوت استطيع تشغيل الموسيقي والفديو في محادثتك الصوتية**
 
 💡 تعلم طريقة تشغيلي واوامر التحكم بي عن طريق  » 📚 الاوامر !
@@ -72,7 +71,7 @@ async def start_(client: Client, message: Message):
                         url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
                     )
                 ],
-                [InlineKeyboardButton("❓ طريقة التفعيل", callback_data="cbhowtouse")],
+                [InlineKeyboardButton("", callback_data="cbhowtouse")],
                 [
                     InlineKeyboardButton("📚 الاوامر", callback_data="cbcmds"),
                     InlineKeyboardButton("❤️ المطور", url=f"https://t.me/{OWNER_NAME}"),
@@ -87,7 +86,7 @@ async def start_(client: Client, message: Message):
                 ],
                 [
                     InlineKeyboardButton(
-                        "نصب بوتك من هنا", url="https://t.me/FA9SH/2059"
+                        "• نصب بوتك من هنا •", url="https://t.me/FA9SH/2059"
                     )
                 ],
             ]
